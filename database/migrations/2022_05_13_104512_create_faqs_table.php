2a103ddbe2d4ad6e6e30b75738ee8790fa2714ee
Harun Selçuk Çetin <harunselcukctn@gmail.com>
2022-05-13 13:51:59 +0300

    create some migrations
    create some migrations

---
+++ database/migrations/2022_05_13_104512_create_faqs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('faqs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('question');
+            $table->foreignId('answer');
+            $table->string('status',length: 6)->default('True');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('faqs');
+    }
+};
---
+++ database/migrations/2022_05_13_104542_create_messages_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('name');
+            $table->foreignId('email');
+            $table->string('phone')->nullable();
+            $table->string('subject')->nullable();
+            $table->string('message');
+            $table->string('ip')->nullable();
+            $table->string('status',length: 6)->default('True');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+};
